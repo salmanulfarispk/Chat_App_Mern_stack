a471468b58d14a19cfca5c0e390fdc2ade347f48
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-06-04 10:56:01 +0530

    creates react and tailwind and also components and routing

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import Allroutes from "./routes/Allroutes.jsx"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={Allroutes}>
+    <App />
+    </RouterProvider>
+  </React.StrictMode>,
+)
---
+++ client/src/routes/Allroutes.jsx
@@ -0,0 +1,39 @@
+import {createBrowserRouter} from "react-router-dom"
+import App from "../App"
+import Register from "../pages/Register"
+import Password from "../pages/Password"
+import EmailPage  from "../pages/EmailPage"
+import Home from "../pages/Home"
+import Message from "../components/Message"
+
+
+
+
+const router= createBrowserRouter([
+    {
+        path:"/",
+        element: <App/>,
+        children: [
+            {
+                path:"/register",
+                element: <Register/>
+            },
+            {
+                path:"/emailpage",
+                element:<EmailPage/>
+            },
+            {
+                path:"/passwordpage",
+                element:<Password/>
+            },
+            {
+                path:"",
+                element:<Home/>,
+                children:[{
+                    path:"/:userId",
+                    element:<Message/>
+                }]
+            }
+        ]
+    }
+])
